13daa5bd15ff3ea74335200dc179f1e5396d3773
MirkoDuric <mirkodjuricczv@gmail.com>
2023-08-12 04:56:00 +0200

    Recipes modul improved

--- Recipes.js
+++ Recipes.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express.Router();
 const pool = require("./pool");
 
+const dishTypeMap = {
+  AllRecipes: [],
+  PeruvianDishes: [1, 2, 3],
+  ZAFDishes: [4, 5, 6],
+  SerbianDishes: [7, 8, 9],
+};
+
 app.get("/:dishType", (req, res) => {
-  let dishType = req.params.dishType;
-  if (dishType === "AllRecipes") {
+  const dishType = req.params.dishType;
+  const dishIds = dishTypeMap[dishType];
+
+  if (!dishIds) {
+    return res.status(404).json({ error: "Unknown dish type requested." });
+  }
+
+  if (dishIds.length === 0) {
     pool
       .query("SELECT id, name, description, image_url FROM dishes;")
       .then(({ rows }) => res.json(rows))
@@ -12,36 +25,11 @@ app.get("/:dishType", (req, res) => {
         console.error(err);
         res.sendStatus(500);
       });
-  }
-  if (dishType === "PeruvianDishes") {
+  } else {
     pool
       .query(
-        "SELECT id,name,description,image_url FROM dishes WHERE id IN ($1, $2, $3);",
-        [1, 2, 3]
-      )
-      .then(({ rows }) => res.json(rows))
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  }
-  if (dishType === "ZAFDishes") {
-    pool
-      .query(
-        "SELECT id,name,description,image_url FROM dishes WHERE id IN ($1, $2, $3);",
-        [4, 5, 6]
-      )
-      .then(({ rows }) => res.json(rows))
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
-  }
-  if (dishType === "SerbianDishes") {
-    pool
-      .query(
-        "SELECT id,name,description,image_url FROM dishes WHERE id IN ($1, $2, $3);",
-        [7, 8, 9]
+        `SELECT id, name, description, image_url FROM dishes WHERE id IN ($1, $2, $3);`,
+        dishIds
       )
       .then(({ rows }) => res.json(rows))
       .catch((err) => {
@@ -50,26 +38,38 @@ app.get("/:dishType", (req, res) => {
       });
   }
 });
+
 app.get("/:dishId/data", (req, res) => {
-  let dishId = req.params.dishId;
+  const dishId = req.params.dishId;
   pool
     .query("SELECT * FROM dishes WHERE id=$1;", [dishId])
     .then(({ rows }) => res.json(rows))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
+
 app.get("/:dishId/ingredients", (req, res) => {
-  let dishId = req.params.dishId;
+  const dishId = req.params.dishId;
   pool
     .query("SELECT * FROM ingredients WHERE dish_id=$1;", [dishId])
     .then(({ rows }) => res.json(rows))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
+
 app.get("/:dishId/preparation", (req, res) => {
-  let dishId = req.params.dishId;
+  const dishId = req.params.dishId;
   pool
     .query("SELECT * FROM preparation_steps WHERE dish_id=$1;", [dishId])
     .then(({ rows }) => res.json(rows))
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = app;
