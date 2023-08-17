94864d21ab26d86c956558d134fc600988f3668a
MirkoDuric <mirkodjuricczv@gmail.com>
2023-08-18 00:09:48 +0200

    New try to fix the error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path"); // Dodato
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -14,19 +13,8 @@ const corsOptions = {
   methods: ["GET", "POST"],
   optionsSuccessStatus: 204,
 };
-
 app.use(cors(corsOptions));
 app.use("", Recipes);
-
-// Ovo je vaš statički direktorijum za React aplikaciju.
-// Posluži statičke fajlove iz 'build' direktorijuma
-app.use(express.static(path.join(__dirname, "build")));
-
-// Za sve nepostojeće rute, posluži React aplikaciju.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Port listens on port ${PORT}`);
 });
