793a04b436e140541e442fb03b02374f8828c0eb
MirkoDuric <mirkodjuricczv@gmail.com>
2023-08-17 22:43:26 +0200

    Server.js updated to fix 404 Errors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path"); // Dodato za putanje
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -14,7 +15,24 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 app.use(cors(corsOptions));
-app.use("", Recipes);
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, "build")));
+
+// Koristi API rute
+app.use("/api", Recipes); // Ovo je promenjeno da bi jasno razdvojili API rute
+
+// Sve ostale rute vodi na index.html
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Middleware za obradu grešaka
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(PORT, () => {
   console.log(`Port listens on port ${PORT}`);
 });
